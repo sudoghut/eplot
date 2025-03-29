82fab8d3d98128a3284cb2e27a5854ef144070dd
Hongsu Wang <sudospace@gmail.com>
2025-03-29 00:32:48 -0400

    update sitemap config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: siteConfig.site,
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), sitemap()],
   markdown: {
     rehypePlugins: [
       [rehypePrettyCode, {
